8aa177dad30fed733a19a0fa2cc9dfa8ec42c7b8
Evan Sun <csumissu@icloud.com>
2021-05-27 23:26:09 +0800

    remove graphql playground

--- graph/router.go
+++ graph/router.go
@@ -2,7 +2,6 @@ package graph
 
 import (
 	"github.com/99designs/gqlgen/graphql/handler"
-	"github.com/99designs/gqlgen/graphql/playground"
 	"github.com/csumissu/SkyDisk/graph/generated"
 	"github.com/gin-gonic/gin"
 )
@@ -13,7 +12,6 @@ func InitRouter() *gin.Engine {
 	v1 := r.Group("/api/v1")
 	{
 		v1.POST("/graphql", graphqlHandler())
-		v1.GET("/playground", playgroundHandler(v1))
 	}
 
 	return r
@@ -21,13 +19,6 @@ func InitRouter() *gin.Engine {
 
 func graphqlHandler() gin.HandlerFunc {
 	h := handler.NewDefaultServer(generated.NewExecutableSchema(generated.Config{Resolvers: &Resolver{}}))
-	return func(c *gin.Context) {
-		h.ServeHTTP(c.Writer, c.Request)
-	}
-}
-
-func playgroundHandler(group *gin.RouterGroup) gin.HandlerFunc {
-	h := playground.Handler("Graphql", group.BasePath()+"/graphql")
 	return func(c *gin.Context) {
 		h.ServeHTTP(c.Writer, c.Request)
 	}
