7602282bcb1ed550b9e450092cccc407802a6a39
Yaxi Sun <yaxi.sun@prudential.com.cn>
2021-06-30 19:25:55 +0800

    run go mod tidy when execute go.sh

--- go.sh
+++ go.sh
@@ -4,6 +4,7 @@ go get github.com/99designs/gqlgen/internal/imports@v0.13.0
 go get github.com/99designs/gqlgen/internal/code@v0.13.0
 go get github.com/99designs/gqlgen/cmd@v0.13.0
 go run github.com/99designs/gqlgen
+go mod tidy
 
 docker compose up -d
 go run main.go
